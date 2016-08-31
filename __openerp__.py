8938f738677a0eefd3ddbc2e45bc03ed26762bf9
AfroMonkey <moisalejandro@gmail.com>
2016-08-31 16:21:13 -0500

    [FIX] depends

--- __openerp__.py
+++ __openerp__.py
@@ -3,7 +3,7 @@
     'version': '1.0',
     'author': 'Humanytek',
     'website': 'http://humanytek.com',
-    'depends': [],
+    'depends': ['stock'],
     'data': [
         'security/ir.model.access.csv',
         'views/vehicle.xml',
